a8c28bb2f73ede823827dac4b47369883af9576f
Koen Reinders <koenf.reinders@gmail.com>
2025-04-17 17:59:52 +0200

    Add workaround for rust-analyzer crashing when loading crates

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -55,3 +55,15 @@ lspconfig.omnisharp.setup({
   capabilities = nvlsp.capabilities,
   cmd = { "/home/koen/.local/share/nvim/mason/bin/omnisharp" }
 })
+
+
+-- Workaround for rust-analyzer diagnostic crash (code -32802)
+for _, method in ipairs({ "textDocument/diagnostic", "workspace/diagnostic" }) do
+  local default_diagnostic_handler = vim.lsp.handlers[method]
+  vim.lsp.handlers[method] = function(err, result, context, config)
+    if err ~= nil and err.code == -32802 then
+      return
+    end
+    return default_diagnostic_handler(err, result, context, config)
+  end
+end
