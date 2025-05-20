9ab14b8086d35c03a7b230ba9344ce7ff6a91269
Koen Reinders <koenf.reinders@gmail.com>
2025-05-20 14:17:29 +0200

    Remove rustvim and add mappings for LSP actions in rustaceanvim

--- lua/plugins/rustaceanvim.lua
+++ lua/plugins/rustaceanvim.lua
@@ -1,5 +1,33 @@
 return {
-  'mrcjkb/rustaceanvim',
-  version = '^5', -- Recommended
-  lazy = false, -- This plugin is already lazy
+  "mrcjkb/rustaceanvim",
+  version = "^5",
+  lazy = false,
+  ft = { "rust" },
+  config = function()
+    vim.g.rustaceanvim = {
+      server = {
+        on_attach = function(client, bufnr)
+          local buf_map = function(mode, lhs, rhs, desc)
+            vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc })
+          end
+
+          -- Code actions
+          buf_map("n", "<leader>la", vim.lsp.buf.code_action, "LSP: Code Action")
+          buf_map("v", "<leader>la", vim.lsp.buf.code_action, "LSP: Code Action")
+          buf_map("n", "<leader>ln", vim.lsp.buf.rename, "LSP: Rename Symbol")
+
+          -- Format on save
+          if client.server_capabilities.documentFormattingProvider then
+            vim.api.nvim_create_autocmd("BufWritePre", {
+              buffer = bufnr,
+              callback = function()
+                vim.lsp.buf.format({ async = false })
+              end,
+            })
+          end
+        end,
+      },
+    }
+  end,
 }
+
